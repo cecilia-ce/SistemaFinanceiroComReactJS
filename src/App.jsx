50c436ba605b7be2342d262b700c83edc089a720
Cecília Albuquerque <90211249+cecilia-ce@users.noreply.github.com>
2022-11-25 00:04:27 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/header';
+import GlobalStyle from './style/global';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+
+  );
+};
+
+export default App;
---
+++ src/components/header/styles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 20vh;
+  text-align: center;
+  background: teal;
+`;
+
+export const Header = styled.h1`
+  font-size: 
+`;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
