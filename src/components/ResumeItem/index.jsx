0d680226796bda9cccdde605129180446c5afc23
Cecília Albuquerque <90211249+cecilia-ce@users.noreply.github.com>
2023-01-09 11:50:59 +0000

    Adicionado formulário de entrada e saída

--- src/components/ResumeItem/index.jsx
+++ src/components/ResumeItem/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import * as C from './styles';
 
-const ResumeItem = () => {
-  return <div>ResumeItem</div>
+const ResumeItem = ({ title, Icon, value }) => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderTitle>{title}</C.HeaderTitle>
+        <Icon />
+      </C.Header>
+      <C.Total>{value}</C.Total>
+    </C.Container>
+  );
 };
 
 export default ResumeItem;
--- src/components/ResumeItem/styles.jsx
+++ src/components/ResumeItem/styles.jsx
@@ -31,4 +31,20 @@ export const Header = styled.header`
   justify-content: space-around;
   width: 100%;
   gap: 10px;
+  margin: 20px auto;
+
+  svg{
+    width: 25px;
+    heigth: 25px; 
+  }
 `;
+
+export const HeaderTitle = styled.p`
+  font-size: 20px;
+`;
+
+export const Total = styled.span`
+  font-size: 30px;
+  font-weight: bold;
+`;
+
